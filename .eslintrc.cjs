423e3cb9740df1f2f3d6d73f011cec6aab8e36bf
tanukile <backpacker320@gmail.com>
2024-02-07 02:34:27 +0800

    chore: Vue vite 與套件

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.vue'],
+      },
+    },
+    'import/core-modules': ['vite', '@vitejs/plugin-vue'],
+  },
+};
